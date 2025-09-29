5e5cbf94894986800a40b1da57db238f2b03ef79
Siri Madhuri <126378302+sirimadhuri02@users.noreply.github.com>
2025-09-29 12:26:56 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,16 @@
 
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db.js";
+import insightsRoute from "./routes/insights.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use("/api/insights", insightsRoute);
+
+const PORT = process.env.PORT || 5000;
+connectDB().then(() =>
+  app.listen(PORT, () => console.log(`🚀 Backend running on ${PORT}`))
+);
