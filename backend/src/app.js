5d6bc97ec0abe82aefcf73608618e86a039dabaf
Siri Madhuri <126378302+sirimadhuri02@users.noreply.github.com>
2025-09-29 14:54:24 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
 import insightsRoute from "./routes/insights.js";
+import cors from "cors";
+app.use(cors());
+
 
 dotenv.config();
 
